225ef3f0ccba625f736b41b0e7c9dff6209cb7ee
ChanwooLee <lcw3176@naver.com>
2022-10-27 17:42:57 +0900

    feat: 결과 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,14 @@
 package onboarding;
 
+import onboarding.problem2.CipherText;
+import onboarding.problem2.DecryptionUtil;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        CipherText ciphertext = CipherText.builder()
+            .value(cryptogram)
+            .build();
+
+        return DecryptionUtil.decrypt(ciphertext).getValue();
     }
 }

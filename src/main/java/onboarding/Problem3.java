6f7af42d5afe94f6be46efb939ed85766e7d6c50
ChanwooLee <lcw3176@naver.com>
2022-10-28 16:35:26 +0900

    feat: 예외 상황 처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,10 @@ import onboarding.problem3.ThreeSixNine;
 
 public class Problem3 {
     public static int solution(int number) {
+        if(number < 1 || number > 10000){
+            throw new IllegalArgumentException("유효하지 않은 게임 범위");
+        }
+
         IMiniGame<ThreeSixNine> game = new ThreeSixNineGame();
         ThreeSixNine result = game.run(ThreeSixNine.builder()
                         .startClapCount(0)

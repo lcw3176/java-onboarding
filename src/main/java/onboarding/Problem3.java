1d5f634c5f3b9b7ad775dfa918c0781ba128ab8b
ChanwooLee <lcw3176@naver.com>
2022-10-28 16:34:24 +0900

    feat: 결과 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import onboarding.problem3.IMiniGame;
+import onboarding.problem3.ThreeSixNineGame;
+import onboarding.problem3.ThreeSixNine;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        IMiniGame<ThreeSixNine> game = new ThreeSixNineGame();
+        ThreeSixNine result = game.run(ThreeSixNine.builder()
+                        .startClapCount(0)
+                        .executedClapCount(0)
+                        .endClapCount(number)
+                        .build());
+
+        return result.getExecutedClapCount();
     }
 }

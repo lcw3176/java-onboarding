9f2b34c153aeb4b9848501964b3988f756005200
ChanwooLee <lcw3176@naver.com>
2022-10-29 20:49:38 +0900

    feat: 결과 값 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+
+import onboarding.problem5.AccountRequest;
+import onboarding.problem5.AccountResponse;
+import onboarding.problem5.IPolicy;
+import onboarding.problem5.PolicyFactory;
+import onboarding.problem5.RequestType;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        IPolicy policy = PolicyFactory.Lightweight.getPolicy();
+        AccountRequest request = new AccountRequest(money, RequestType.WITHDRAW);
+        AccountResponse response = policy.withdraw(request);
+
+        return response.getResultList();
     }
 }

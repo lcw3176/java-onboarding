f6a73a853b0622cbbdd191e32bb0d93894e1ef6f
ChanwooLee <lcw3176@naver.com>
2022-10-31 00:09:09 +0900

    feat: 예외 처리 메인 솔루션에 적용

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import onboarding.problem6.Crew;
+import onboarding.problem6.EmailValidator;
+import onboarding.problem6.ICrewValidator;
 import onboarding.problem6.NicknameFilter;
+import onboarding.problem6.NicknameValidator;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -13,6 +16,16 @@ public class Problem6 {
 
         for(List<String> i : forms){
             crewList.add(new Crew(i.get(0), i.get(1)));
+        }
+
+        List<ICrewValidator> validatorList = new LinkedList<>();
+        validatorList.add(new EmailValidator());
+        validatorList.add(new NicknameValidator());
+
+        for(ICrewValidator i : validatorList){
+            if(!i.isValid(crewList)){
+                throw new IllegalArgumentException("유효하지 않은 파라미터");
+            }
         }
 
         NicknameFilter nicknameFilter = new NicknameFilter();

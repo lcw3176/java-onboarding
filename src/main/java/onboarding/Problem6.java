1e2e417061ece39cb0ee339a77edac24a448f392
ChanwooLee <lcw3176@naver.com>
2022-10-31 00:06:39 +0900

    feat: 메일 발송 대상에 해당되는 크루 추출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import onboarding.problem6.Crew;
+import onboarding.problem6.NicknameFilter;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<Crew> crewList = new LinkedList<>();
+
+        for(List<String> i : forms){
+            crewList.add(new Crew(i.get(0), i.get(1)));
+        }
+
+        NicknameFilter nicknameFilter = new NicknameFilter();
+        List<Crew> survivorList = nicknameFilter.doFilter(new LinkedList<>(crewList));
+        crewList.removeAll(survivorList);
+
+        return null;
     }
 }

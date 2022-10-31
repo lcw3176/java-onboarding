0a6184429f658d5b2fde85a025846712d829a2a4
ChanwooLee <lcw3176@naver.com>
2022-10-31 17:40:32 +0900

    feat: 각각 유저에게 친구 목록 추가

---
+++ src/main/java/onboarding/problem7/CrewRecommender.java
@@ -0,0 +1,31 @@
+package onboarding.problem7;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class CrewRecommender {
+
+	public void relateFriendship(List<String> crewList, Map<String, Crew> crewMap){
+		String firstFriendName = crewList.get(0);
+		String secondFriendName = crewList.get(1);
+
+		Crew firstCrew = crewMap.get(firstFriendName);
+		Crew secondCrew = crewMap.get(secondFriendName);
+
+		crewMap.get(firstFriendName).addKnownFriend(secondCrew);
+		crewMap.get(secondFriendName).addKnownFriend(firstCrew);
+	}
+
+
+	public void addCrewToMap(List<String> crewList, Map<String, Crew> crewMap){
+		for(String i : crewList){
+			if(!crewMap.containsKey(i)){
+				crewMap.put(i, new Crew(i,0, new HashSet<>()));
+			}
+		}
+	}
+
+
+
+}
